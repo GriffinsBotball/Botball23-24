1eff70c1b1c8c579f6c161d2a529e7551a61628f
HalfMeridian <145166368+HalfMeridian@users.noreply.github.com>
2024-01-12 16:39:05 -0600

    Update Demobot Code.c
    Ensure the code that was used at the competition is the code found here. This is for the fall competition.

--- Fall Competition/Demobot Code.c
+++ Fall Competition/Demobot Code.c
@@ -3,36 +3,58 @@
 
 int main()
 {	
+    //Start up
+    enable_servos();
+    set_servo_position(3,1700);
+    set_servo_position(1,1500);
+    wait_for_light(5);
+  	shut_down_in(115);
     
     //Square up at start of game
-    enable_servos();
-    set_servo_position(2,1700);
-    set_servo_position(1,1500);
     squareUpReverse();
     
     //Wack the people into Zone B
-    set_servo_position(2,700); 
+    set_servo_position(3,700); 
     driveDistance(3);
     turnDegrees(-90);
     squareUpReverse();//
-    driveDistance(5);//Square up at the begining
+    driveDistance(3.5);//Square up at the begining
     
     set_servo_position(1,0);
     msleep(1000);
-    set_servo_position(2,0);
+    set_servo_position(3,185);
     msleep(1000);
-    turnDegrees(90);
-    driveDistance(10);
+    turnDegrees(89);
+    squareUpReverse();
+    
+    
+    // Get Botguy
+    driveDistance(53);
+    turnDegrees(-1);
+	ao();
+    msleep(1000);
+    enable_servos();
+    set_servo_position(1,2000);
+    msleep(1000);
+    driveDistance(-20);
+    set_servo_position(1,0);
+    msleep(1000);
+    set_servo_position(3,700);
+    turnDegrees(5.5);
+    squareUpReverse();
     
     //square back up to get orange poms
     set_servo_position(1,1500);
+    set_servo_position(3,1400);
     squareUpReverse();
     turnDegrees(-90);
     squareUpReverse();
     turnDegrees(90);
     squareUpReverse();
+   
     
     // Find second black line from starting box
+    turnDegrees(2.5);
     driveDistance(45); 
     msleep(100);
     blackLineDetect(3800);
@@ -40,56 +62,57 @@ int main()
     
     // To here, good so far!
     
-    
     //Travel near tall cup
-    driveDistance(51);
+    driveDistance(47);
     turnDegrees(80);
     msleep(50);
     printf("Travel to Cup \n");
     
     //Aim for cup with the arm
-    set_servo_position(1, 40);
-    set_servo_position(2, 400);
-    msleep(1000);
-    driveDistance(17);
-    msleep(200);
+    set_servo_position(1, 750);
+    msleep(2000);
+    turnDegrees(-2.5);
+    set_servo_position(3, 250);
+    msleep(2000);
+    driveDistance(4.25);// was 17.5
+    msleep(2000);
     printf("Aim for Cup \n");
    
     //Grab cup and reverse
-   	set_servo_position(2, 2040);
+   	set_servo_position(1, 2000);
     msleep(500);
-    driveDistance(-15);
+    driveDistance(-20);
     printf("Grabbed Cup \n");
     
     //Turn towards start box, drive forward to Zone B, Lift PVC holder.
-    turnDegrees(105);
+    turnDegrees(110);
     driveDistance(30);
     turnDegrees(-30);
     driveDistance(25);
     
     //lifts the claw to drop the poms
-    set_servo_position(1, 40);
+    set_servo_position(3, 40);
     msleep(200);
-    set_servo_position(1, 150);
+    set_servo_position(3, 700);
     msleep(200);
-    set_servo_position(1, 70);
+    set_servo_position(3, 70);
     msleep(200);
     
     //turns to position poms
-    set_servo_position(1, 250);
+    set_servo_position(3, 250);
     msleep(200);
     turnDegrees(10);
-    set_servo_position(1, 150);
+    set_servo_position(3, 150);
     msleep(200);
     turnDegrees(10);
-    set_servo_position(1,1000);
+    set_servo_position(3,1000);
     msleep(1000);
     
     //sets down container
     set_servo_position(1,150);
-    set_servo_position(2, 300);
+    set_servo_position(3, 300);
     printf("Driving towards start box. \n");
-    set_servo_position(1,1000);
+    set_servo_position(3,1000);
     msleep(1000);
     
     turnDegrees(-45);
